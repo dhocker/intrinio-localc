8fcb169ebff1fb4ee8d82a961ce25573ce7d0893
Dave Hocker <qalydon17@gmail.com>
2017-09-12 13:52:57 -0500

    Update email address in copyrigh statement.

--- xcu_file.py
+++ xcu_file.py
@@ -1,6 +1,6 @@
 #
 # Create a component .xcu file describing its functions
-# Copyright (C) 2017  Dave Hocker (email: AtHomeX10@gmail.com)
+# Copyright (C) 2017  Dave Hocker (email: qalydon17@gmail.com)
 #
 # This program is free software: you can redistribute it and/or modify
 # it under the terms of the GNU General Public License as published by
